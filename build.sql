e4e79dff29f234f55e4b1c2cbfc4b752add488af
laihl <498295324@qq.com>
2019-01-06 14:17:30 +0800

    新增用户扩展表

--- build.sql
+++ build.sql
@@ -1,12 +1,16 @@
+#数据库参数初始化
 set names utf8;
 set character_set_server =utf8;
 set character_set_client =utf8;
 set character_set_database=utf8;
 set character_set_filesystem=utf8;
 show variables  like 'char%';
-
+#创建数据库
+drop database if exists test;
 create database test character set utf8 ;
 
+#用户表创建
+drop table if exists t_user;
 create table t_user(
   user_id int auto_increment primary key ,
   user_name varchar(50),
@@ -14,4 +18,10 @@ create table t_user(
   user_pwd varchar(100)
 );
 
+drop table if exists t_user_ext;
+create table t_user_ext(
+  ext_id int auto_increment primary key ,
+  reward float(1,3) default 0.0,
+  mission int default 0
+);
 
